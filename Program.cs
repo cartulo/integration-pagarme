c2e69d86d91ade17b390ad76b89842d19a255a02
Gilberto Júnior <contato.gil.tulo@gmail.com>
2023-04-18 10:42:34 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using RestSharp;
+
+var client = new RestClient("https://api.pagar.me/core/v5");
+var request = new RestRequest("plans", Method.Post);
+
+request.AddHeader("accept", "application/json");
+request.AddHeader("content-type", "application/json");
+request.AddHeader("authorization", "Basic c2tfdGVzdF83ekpBR3JqU0dpMkdvNnhPOg==");
+
+var requestBody = new
+{
+    interval = "month",
+    interval_count = 1,
+    pricing_scheme = new
+    {
+        scheme_type = "Unit",
+        price = 10
+    },
+    quantity = 1,
+    name = "Plano Teste"
+};
+
+request.AddJsonBody(requestBody);
+
+var response = client.Post(request);
+
+Console.WriteLine("StatusCode: " + response.StatusCode);
